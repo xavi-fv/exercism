29858ff214e96b6175f22e7772b3c2e60b7422e8
Xavi <xavifv@pm.me>
2023-02-18 10:12:40 +0100

    Added hello world + lucians exercises

---
+++ rust/lucians-luscious-lasagna/src/lib.rs
@@ -0,0 +1,19 @@
+const EXPECTED_MINUTES_IN_OVEN: i32 = 40;
+const MINUTES_PER_LAYER: i32 = 2;
+
+
+pub fn expected_minutes_in_oven() -> i32 {
+    EXPECTED_MINUTES_IN_OVEN
+}
+
+pub fn remaining_minutes_in_oven(actual_minutes_in_oven: i32) -> i32 {
+    expected_minutes_in_oven() - actual_minutes_in_oven
+}
+
+pub fn preparation_time_in_minutes(number_of_layers: i32) -> i32 {
+    number_of_layers * MINUTES_PER_LAYER
+}
+
+pub fn elapsed_time_in_minutes(number_of_layers: i32, actual_minutes_in_oven: i32) -> i32 {
+    preparation_time_in_minutes(number_of_layers) + actual_minutes_in_oven
+}
